73af4adfff0972dbe758459de179f51acc8d75cc
laronandrew11 <laronandrew11@yahoo.com>
2015-01-24 08:50:40 +0800

    Corrected save file writing format; moved saveGame() function to main.js

--- scripts/main.js
+++ scripts/main.js
@@ -30,6 +30,51 @@ var buttonBG = document.createElement('img');
 //var activeScreen;
 
 var gameState;
+
+
+
+function saveGame(){
+	var saveText='{"gameState":{"playerName":"'+gameState.playerName+'","money":'+gameState.money+',"mapX":'+gameState.mapX+',"mapY":'+gameState.mapY+',"usedCapacity":'+gameState.usedCapacity+',"settlement":"'+gameState.settlement+'",';
+	var i;
+
+	var shipsString='"ships":[';
+	for(i=0;i<gameState.ships.length;i++)
+	{
+		shipsString=shipsString+'{"type":"'+	gameState.ships[i].type+'", "speed":'+gameState.ships[i].speed+', "health": '+gameState.ships[i].health+', "cargoCapacity":'+gameState.ships[i].cargoCapacity+'}';
+		if(i<gameState.ships.length-1)
+			shipsString=shipsString+', ';
+	}
+	shipsString=shipsString+'],';
+
+	var cargoString='"cargo":[';
+	for(i=0;i<gameState.cargo.length;i++)
+	{
+		cargoString=cargoString+'{"name":"'+	gameState.cargo[i].name+'", "type":"'+gameState.cargo[i].type+'", "unitWeight": '+gameState.cargo[i].unitWeight+', "amount":'+gameState.cargo[i].amount+'}';
+		if(i<gameState.cargo.length-1)
+			cargoString=cargoString+', ';
+	}
+	cargoString=cargoString+'],';
+
+	var visibleSettlementsString='"visibleSettlements":[';
+	for(i=0;i<gameState.visibleSettlements.length;i++)
+	{
+		visibleSettlementsString=visibleSettlementsString+'"'+gameState.visibleSettlements[i]+'"';
+		if(i<gameState.visibleSettlements.length-1)
+			visibleSettlementsString=visibleSettlementsString+', ';
+	}
+	visibleSettlementsString=visibleSettlementsString+'],';
+
+	var gameDateString='"gameDate":{"year":'+gameState.gameDate.year+',"month":'+gameState.gameDate.month+',"day":'+gameState.gameDate.day+'}';
+
+	saveText=saveText+shipsString+cargoString+visibleSettlementsString+gameDateString+'}}';
+
+	console.log(saveText);
+	//append gameState.quests;
+
+}
+
+
+
 //TODO optimize screen switching by not re-initializing screens every time we switch screens, or removing invisible screens from memory
 function main()
 {
--- scripts/parsers_writers/saveGameParser.js
+++ scripts/parsers_writers/saveGameParser.js
@@ -1,11 +1,11 @@
 function saveGame(){
-	var saveText='{"gameState":{playerName":'+gameState.playerName+',"money":'+gameState.money+'",mapX":'+gameState.mapX+'",mapY":'+gameState.mapY+'",usedCapacity":'+gameState.usedCapacity+'",settlement:"+'gameState.settlement+',';
+	var saveText='{"gameState":{"playerName":"'+gameState.playerName+'","money":'+gameState.money+',"mapX":'+gameState.mapX+',"mapY":'+gameState.mapY+',"usedCapacity":'+gameState.usedCapacity+',"settlement":"'+gameState.settlement+'",';
 	var i;
 
 	var shipsString='"ships":[';
 	for(i=0;i<gameState.ships.length;i++)
 	{
-		shipsString=shipsString+'{"type":'+	gameState.ships[i].type+', "speed":'+gameState.ships[i].speed+', "health": '+gameState.ships[i].health+', "cargoCapacity":'+gameState.ships[i].cargoCapacity+'}');
+		shipsString=shipsString+'{"type":"'+	gameState.ships[i].type+'", "speed":'+gameState.ships[i].speed+', "health": '+gameState.ships[i].health+', "cargoCapacity":'+gameState.ships[i].cargoCapacity+'}';
 		if(i<gameState.ships.length-1)
 			shipsString=shipsString+', ';
 	}
@@ -14,7 +14,7 @@ function saveGame(){
 	var cargoString='"cargo":[';
 	for(i=0;i<gameState.cargo.length;i++)
 	{
-		cargoString=cargoString+'{"name":'+	gameState.cargo[i].name+', "type":'+gameState.cargo[i].type+', "unitWeight": '+gameState.cargo[i].unitWeight+', "amount":'+gameState.cargo[i].amount+'}');
+		cargoString=cargoString+'{"name":"'+	gameState.cargo[i].name+'", "type":"'+gameState.cargo[i].type+'", "unitWeight": '+gameState.cargo[i].unitWeight+', "amount":'+gameState.cargo[i].amount+'}';
 		if(i<gameState.cargo.length-1)
 			cargoString=cargoString+', ';
 	}
@@ -23,7 +23,7 @@ function saveGame(){
 	var visibleSettlementsString='"visibleSettlements":[';
 	for(i=0;i<gameState.visibleSettlements.length;i++)
 	{
-		visibleSettlementsString=visibleSettlementsString+visibleSettlements[i];
+		visibleSettlementsString=visibleSettlementsString+'"'+gameState.visibleSettlements[i]+'"';
 		if(i<gameState.visibleSettlements.length-1)
 			visibleSettlementsString=visibleSettlementsString+', ';
 	}
