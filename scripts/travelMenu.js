722183e852a4a8036eaab553818bd4b8e2fe8a2a
laronandrew11 <laronandrew11@yahoo.com>
2015-03-07 11:44:05 +0800

    added money/player name labels to travel screen

--- scripts/travelMenu.js
+++ scripts/travelMenu.js
@@ -5,8 +5,15 @@ function addTravelMenu(){
 	addSettlementMarkers(settlementFetcher, travelScreen);
 		//pnlMap.addButton(newButton);
 	
-	var lblGameDate=new Label(700,36,100,50,gameState.gameDate.year+"-"+gameState.gameDate.month+"-"+gameState.gameDate.day,"Epistolar",36,"black");
+	
+
+	var lblPlayerName=new Label(700,18,100,50,gameState.playerName,"Epistolar",18,"black");
+	var lblGameDate=new Label(700,38,100,50,gameState.gameDate.year+"-"+gameState.gameDate.month+"-"+gameState.gameDate.day,"Epistolar",18,"black");
+	var lblMoney=new Label(800,38,100,50,gameState.money+" gold","Epistolar",18,"black");
+	
 	travelScreen.addLabel(lblGameDate);
+	travelScreen.addLabel(lblPlayerName);
+	travelScreen.addLabel(lblMoney);
 	travelScreen.initialize();
 }
 	
